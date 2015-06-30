d1f3364d05bc4ea424215473f78d9ac5cb5d6ec1
verdugoperez <manuel.alejandro.verdugo@gmail.com>
2015-06-29 22:38:14 -0600

    Agregue pagina personalizada de error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+
+        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+            	background: #ed1a3b;
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: white;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+            a {color:white;}
+
+            a:hover{
+            	color:black;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Ooops tenemos un error disculpa las molestias!!.  :(</div>
+                <a href="http://localhost:8000/" accesskey="1" title="">Regresar a la página de inicio</a>
+            </div>
+        </div>
+    </body>
+</html>
